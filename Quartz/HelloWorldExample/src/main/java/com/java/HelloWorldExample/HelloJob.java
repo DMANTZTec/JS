17ca09514107193518c29b7b63121f5de9cd2648
GolusulaTejaswini <tejaswini.golusula@gmail.com>
2017-10-17 16:29:54 +0530

    adding jdbc job store configuration

--- Quartz/HelloWorldExample/src/main/java/com/java/HelloWorldExample/HelloJob.java
+++
@@ -1,9 +0,0 @@
-package com.java.HelloWorldExample;
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-
-public class HelloJob implements Job {
-    public void execute(JobExecutionContext context){
-        System.out.println("Hello World !!!");
-    }
-}
